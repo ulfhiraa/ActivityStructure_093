a432a9ffec92d4505abf8cd184c751bfa5aa1010
ulfhiraa <studyiraa@gmail.com>
2023-04-05 11:37:41 +0700

    menggunakan for untuk array

--- ActivityStructure3/ActivityStructure3.cpp
+++ ActivityStructure3/ActivityStructure3.cpp
@@ -16,4 +16,17 @@ struct Mahasiswa
 };
 
 int main() {
-	Mahasiswa mhs[4];
+	Mahasiswa mhs[3];
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Masukkan NIM : ";
+        cin.getline(mhs[i].nim, 12);
+        cout << "Masukkan Umur : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << "\nMasukkan Nama : ";
+        cin.getline(mhs[i].alamat.desa, 20);
+        cout << "\tMasukkan Kota : ";
+        cin.getline(mhs[i].alamat.kota, 20);
+    }
+
