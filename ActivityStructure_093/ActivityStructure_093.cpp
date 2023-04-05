323fefc42e114ace001be4ce23e369625168dd92
ulfhiraa <studyiraa@gmail.com>
2023-04-05 10:42:19 +0700

    Add project files.

---
+++ ActivityStructure_093/ActivityStructure_093.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs, mhs2;
+
+	mhs.nim = "20210140019";
+	mhs.umur = 20;
+	mhs.nama = "Ardian Mulya";
+	mhs.alamat = "Wonosobo";
+
+	cout << "Masukkan NIM    : ";
+	cin >> mhs2.nim;
+	cout << "Masukkan Umur   : ";
+	cin >> mhs2.umur;
+	cout << "Masukkan Nama   : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs2.alamat;
+
+	cout << "NIM : " << mhs.nim;
+	cout << "\nUmur   : " << mhs.umur;
+	cout << "\nNama   : " << mhs.nama;
+	cout << "\nAlamat : " << mhs.alamat;
+
+	cout << "\n\nNIM  : " << mhs2.nim;
+	cout << "\nUmur   : " << mhs2.umur;
+	cout << "\nNama   : " << mhs2.nama;
+	cout << "\nAlamat : " << mhs2.alamat;
+
+}
